c493dc155014ac44eee11a6bb40b803cba70b2a7
tatendakabike <tkabike@gmail.com>
2019-12-06 17:53:09 +0200

    Initial Commit

---
+++ AlamoStarter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamoStarter
+//
+//  Created by Tatenda Kabike on 2019/12/06.
+//  Copyright © 2019 Tatenda Kabike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
